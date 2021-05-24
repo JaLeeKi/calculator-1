874680aadfe8f44ebc051b907fd5d248ab5cf0cb
JaLeeKi <jadekirkham@gmail.com>
2021-05-24 15:51:49 -0600

    Created If statements for calculator

--- calc.js
+++ calc.js
@@ -1,3 +1,4 @@
+const { SSL_OP_NO_QUERY_MTU } = require('constants');
 const readline = require('readline');
 
 const reader = readline.createInterface({
@@ -14,6 +15,25 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
+	if (mathSymbol === "+"){
+			console.log(num1 + num2);
+	}
+
+	if (mathSymbol === "-"){
+		console.log(num1 - num2);
+	}
+
+	if (mathSymbol === "*"){
+		console.log(num1 * num2);
+	}
+
+	if (mathSymbol === "/"){
+		console.log(num1 / num2);
+	}
+
+	if (mathSymbol === "sqrt"){
+		console.log(Math.sqrt(num1));
+	}
 
 	// This line closes the connection to the command line interface.
 	reader.close()
