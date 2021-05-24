35b82f998f037aefe67d0cd3ac7b197efddfc764
JaLeeKi <jadekirkham@gmail.com>
2021-05-24 16:12:30 -0600

    Added in options for 3 numbers and more calculations

--- calc.js
+++ calc.js
@@ -14,25 +14,42 @@ reader.question("What would you like to calculate?", function(input){
 	mathSymbol = tokens[0];
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
+	num3 = Number(tokens[3]);
 
 	if (mathSymbol === "+"){
-			console.log(num1 + num2);
+			console.log(num1 + num2 + num3);
 	}
 
 	if (mathSymbol === "-"){
-		console.log(num1 - num2);
+		console.log(num1 - num2 - num3);
 	}
 
 	if (mathSymbol === "*"){
-		console.log(num1 * num2);
+		console.log(num1 * num2 * num3);
 	}
 
 	if (mathSymbol === "/"){
-		console.log(num1 / num2);
+		console.log(num1 / num2 / num3);
 	}
 
 	if (mathSymbol === "sqrt"){
 		console.log(Math.sqrt(num1));
+	}
+
+	if (mathSymbol === "^"){
+		console.log(Math.pow(num1, 2))
+	}
+	
+	if (mathSymbol === "^^"){
+		console.log(Math.pow(num1, 3))
+	}
+
+	if (mathSymbol === "^^^"){
+		console.log(Math.pow(num1, num2))
+	}
+
+	if (mathSymbol === "%"){
+		console.log(num1 % num2)
 	}
 
 	// This line closes the connection to the command line interface.
